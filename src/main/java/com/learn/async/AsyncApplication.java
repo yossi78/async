b2f98d2d02710c9aaeeec6b29b117a2637875df2
yossi78 <yossi78@gmail.com>
2021-06-20 19:07:05 +0300

    first

--- src/main/java/com/learn/async/AsyncApplication.java
+++ src/main/java/com/learn/async/AsyncApplication.java
@@ -1,8 +1,9 @@
 package com.learn.async;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+
 
 @SpringBootApplication
 @EnableAsync
